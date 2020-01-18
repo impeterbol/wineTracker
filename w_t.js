fccc2c1dfbe78f82f72b622e8ff7ffa3215001c3
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-01-17 21:28:29 -0800

    project was stored locally - uploading to github - need to finish

---
+++ w_t.js
@@ -0,0 +1,60 @@
+$('input[name="bname1"]').keypress(function(event){
+	if (event.which === 13){
+		console.log("Bottle of " + $('input[name="bname1"]').val() + " saved");
+		$("#p1").toggle();
+
+	}
+})
+
+// $('#1').on("click",function()
+// {
+// 	console.log("Bottle of " + $('input[name="bname1"]').val() + " saved");
+// }
+// )
+
+($"#sq1")hide();
+
+
+
+$('#btn2').on("click",function()
+{
+	console.log("Bottle of " + $('input[name="bname2"]').val() + " saved");
+}
+)
+
+
+$('#btn3').on("click",function()
+{
+	console.log("Bottle of " + $('input[name="bname3"]').val() + " saved");
+}
+)
+
+$('#btn4').on("click",function()
+{
+	console.log("Bottle of " + $('input[name="bname4"]').val() + " saved");
+}
+)
+
+$('#btn5').on("click",function()
+{
+	console.log("Bottle of " + $('input[name="bname5"]').val() + " saved");
+}
+)
+
+$('#btn6').on("click",function()
+{
+	console.log("Bottle of " + $('input[name="bname6"]').val() + " saved");
+}
+)
+
+$('#btn7').on("click",function()
+{
+	console.log("Bottle of " + $('input[name="bname7"]').val() + " saved");
+}
+)
+
+$('#btn8').on("click",function()
+{
+	console.log("Bottle of " + $('input[name="bname8"]').val() + " saved");
+}
+)
